6a6273ec5a796a1b8d94f59fbb4964f73167fcca
Nanthiya <6531503041@lamduan.mfu.ac.th>
2023-11-11 15:21:33 +0700

    NanthiKk

--- src/main/java/th/mfu/repository/AdminRepository.java
+++ src/main/java/th/mfu/repository/AdminRepository.java
@@ -1,8 +1,9 @@
 package th.mfu.repository;
 
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AdminRepository extends CrudRepository<Admin,Integer>{
-    
+public interface AdminRepository extends JpaRepository<Admin,Integer>{
+       
 }
