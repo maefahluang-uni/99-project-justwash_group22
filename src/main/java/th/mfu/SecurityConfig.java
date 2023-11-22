265f5d5e3116abce1aa8ec383cb7a273eb467247
Peerawut Nipakornpan <6531503060@lamduan.mfu.ac.th>
2023-11-22 13:28:50 +0700

    login JA

---
+++ src/main/java/th/mfu/SecurityConfig.java
@@ -0,0 +1,29 @@
+package th.mfu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/api/public/**").permitAll() // Allow public access
+                .anyRequest().authenticated() // Require authentication for other requests
+                .and()
+                .logout() // Configure logout
+                    .logoutUrl("/logout") // Set the logout URL
+                    .logoutSuccessUrl("/") // Redirect to home page after logout
+                    .invalidateHttpSession(true) // Invalidate session
+                    .deleteCookies("JSESSIONID") // Delete cookies if needed
+                    .permitAll() // Allow all to access logout URL
+                .and()
+                .csrf().disable() // Disable CSRF (http://www.baeldung.com/spring-security-csrf)
+            .httpBasic(); // Enable Basic Authentication
+    }
+}
